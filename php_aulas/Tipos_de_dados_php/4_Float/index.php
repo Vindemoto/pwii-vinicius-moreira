a9727b3f950a58ac9c4131ebc2d5031fa2703fd0
Vinicius <57239647+Vindemoto@users.noreply.github.com>
2026-08-14 22:26:04 -0300

    Add files via upload

---
+++ php_aulas/Tipos_de_dados_php/4_Float/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Float em PHP</title>
+</head>
+<body>
+    
+<?php
+
+$a = 1.12;
+
+echo $a;
+echo "<br>";
+echo 1.123;
+echo "<br>";
+echo 12.5 + 1.3278;
+echo "<br>";
+echo 12 + 1.3278;
+
+/* Números decimais(floats)
+- Float são todos os números com casas decimais;
+- Separação apenas com . e nunca com ,
+
+EXEMPLO: 2.695, 1.999, 0.6, -8.70; 
+
+*/
+
+?>
+
+
+</body>
+</html>
