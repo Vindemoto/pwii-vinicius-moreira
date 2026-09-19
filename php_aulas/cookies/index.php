9dc77964270103bdea01e8cfa88fac5e6953d083
Vinicius <57239647+Vindemoto@users.noreply.github.com>
2026-09-18 22:27:46 -0300

    Add files via upload

---
+++ php_aulas/cookies/index.php
@@ -0,0 +1,17 @@
+<?php
+include "valida_cookies.inc";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valida Cookies</title>
+</head>
+<body>
+    Seja Bem-vido ao meu site<br>
+    Conteúdo do site aqui<br>
+    <p><a href="logout.php">Logout</a></p>
+</body>
+</html>
